314ca2458d794da05336ba6b2beaad90ae3e4869
Rizki Fiananda <rizkyfiananda@gamil.com>
2025-10-09 22:16:04 +0700

    feat: add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'config.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Portal Booking Pendakian</title>
+</head>
+<body>
+    <h1>Portal Booking Pendakian</h1>
+    <h2>Daftar Gunung</h2>
+    
+
+</body>
+</html>
